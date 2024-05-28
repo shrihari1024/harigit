b2175e300e849d0558a44df932e6832034061760
Shrihari Chandrashekar <shrihari_264@yahoo.co.in>
2024-05-29 00:00:21 +0530

    Reverted Change

--- demo.py
+++ demo.py
@@ -8,4 +8,4 @@ x = np.linspace(0, 20, 100) # Create a list of evenly-spaced numbers over the ra
 plt.plot(x, np.sin(x))      # Plot the sine of each x point
 
 
-plt.showme()                  # Display the plot
+plt.show()                  # Display the plot
